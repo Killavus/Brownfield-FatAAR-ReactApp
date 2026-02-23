88d3bbad390f68df22e8edb862c8dfa6636d9652
Killavus <killavus@gmail.com>
2026-02-23 01:28:07 +0100

    change hermes-android to a proper version

--- android/rnlib/build.gradle.kts
+++ android/rnlib/build.gradle.kts
@@ -97,7 +97,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     api("com.facebook.react:react-android:0.84.0")
-    api("com.facebook.react:hermes-android:0.84.0")
+    api("com.facebook.react:hermes-android:0.14.0")
 }
 
 react {
