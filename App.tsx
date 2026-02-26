b12eef5b77ca81ce757c8ddd92037a2b7cab7ad8
Killavus <killavus@gmail.com>
2026-02-26 19:11:41 +0100

    rely on nitro native pipe

--- App.tsx
+++ App.tsx
@@ -1,9 +1,46 @@
-import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Text, View, StyleSheet, Button, Pressable } from 'react-native';
+import NativePipe from 'react-native-nitro-event-pipe';
+import type { Accommodation } from 'react-native-nitro-event-pipe';
 
 export default function App() {
+  const [accommodations, setAccommodations] = useState<Accommodation[] | null>(
+    null
+  );
+
+  useEffect(() => {
+    NativePipe.onAccommodationMapChange((accomodationMap) => {
+      console.log('accomodation map change...');
+      setAccommodations(accomodationMap.results);
+    });
+  }, []);
   return (
-    <View>
-      <Text>Hello My Brownfield App!</Text>
+    <View style={styles.container}>
+      <Button
+        title="Start"
+        onPress={() => NativePipe.requestAccommodationMap()}
+      />
+
+      {accommodations?.map((accommodation) => (
+        <Pressable
+          style={{ padding: 12 }}
+          key={accommodation.hotelId}
+          onPress={() => NativePipe.selectAccommodation(accommodation.hotelId)}
+        >
+          <Text>{accommodation.name}</Text>
+        </Pressable>
+      ))}
+
+      <Button title="Refresh" onPress={() => NativePipe.refresh()} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
